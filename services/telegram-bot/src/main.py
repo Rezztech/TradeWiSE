24f56d96e55cc856471aab2e0a1421770dc293a2
WildfootW <wildfootw@wildfoo.tw>
2023-12-02 19:44:09 +0800

    telegram-bot: add function request stock price

--- services/telegram-bot/src/main.py
+++ services/telegram-bot/src/main.py
@@ -9,6 +9,7 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
 import os
 
 telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -16,32 +17,34 @@ telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+async def get_stock_price(symbol: str):
+    response = requests.get(f'http://fugle-market-data:80/price/{symbol}')
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {'error': 'Unable to fetch data'}
+
 # Define command handlers
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome to TradeWiSE! Your trading assistant.")
 
-async def stock_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Logic to fetch and display stock data
-    pass
-
-async def trade(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Logic to execute trade orders
-    pass
-
-async def analyze(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Logic for financial data analysis
-    pass
+async def stock(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    args = context.args
+    if not args:
+        await update.message.reply_text("Please provide a stock symbol. Usage: /stock <symbol>")
+        return
+    symbol = args[0]
+    data = await get_stock_price(symbol)
+    await update.message.reply_text(str(data))
 
 # Main function to setup the bot
+#async def main():
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_bot_token).build()
 
     # Register handlers
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('stock', stock_info))
-    application.add_handler(CommandHandler('trade', trade))
-    application.add_handler(CommandHandler('analyze', analyze))
+    application.add_handler(CommandHandler('stock', stock))
 
     # Start the bot
     application.run_polling()
-
