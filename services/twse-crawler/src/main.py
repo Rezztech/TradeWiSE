c8d2f447fcff0027711bb490434a6bc764d738d5
WildfootW <wildfootw@wildfoo.tw>
2023-12-13 20:06:34 +0800

    twse-crawler: not working

--- services/twse-crawler/src/main.py
+++ services/twse-crawler/src/main.py
@@ -15,51 +15,52 @@
 
 import aiohttp
 import asyncio
-import pandas as pd
+import pandas
 from io import StringIO
 import logging
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import functools
 import time
-
-# URLs for different types of financial reports
-REPORT_URLS = {
-    "balance_sheet": "https://mops.twse.com.tw/mops/web/ajax_t164sb03",
-    "income_statement": "https://mops.twse.com.tw/mops/web/ajax_t164sb04",
-    "cash_flow": "https://mops.twse.com.tw/mops/web/ajax_t164sb05"
-}
+import os
+import datetime
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(level=log_level, format="%(asctime)s %(levelname)s %(message)s")
 
 class RateLimiter:
     def __init__(self, calls_per_second):
         self.period = 1.0 / calls_per_second
+        # Create an asyncio lock for synchronizing access in asynchronous context
+        self.lock = asyncio.Lock()
         self.last_called = time.monotonic()
 
-    async def __aenter__(self):
-        current = time.monotonic()
-        sleep_for = self.last_called + self.period - current
-        if sleep_for > 0:
-            await asyncio.sleep(sleep_for)
-        self.last_called = time.monotonic()
+    async def wait(self):
+        # Acquire the lock to ensure exclusive access to check/update last_called time
+        async with self.lock:
+            current = time.monotonic()
+            sleep_for = self.last_called + self.period - current
+            if sleep_for > 0:
+                logging.debug(f"Rate limiter active, sleeping for {sleep_for} seconds")
+                await asyncio.sleep(sleep_for)
+            self.last_called = time.monotonic()
+            logging.debug("Rate limiter lock released")
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        pass
+limiter = RateLimiter(calls_per_second=0.1) # Global instance of RateLimiter
+semaphore = asyncio.Semaphore(5) # max_concurrent_tasks
 
-def rate_limited(calls_per_second):
-    limiter = RateLimiter(calls_per_second)
 
-    def decorator(func):
-        @functools.wraps(func)
-        async def wrapped(*args, **kwargs):
-            async with limiter:
-                return await func(*args, **kwargs)
-        return wrapped
-    return decorator
+def rate_limited(func):
+    # Use functools.wraps to preserve metadata of the original function
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs):
+        await limiter.wait()
+        # Call the original function and return its result
+        return await func(*args, **kwargs)
+    return wrapped
 
-# Async function to extract report data
-async def extract_report_data(html_dataframe, year, season, report_type):
+# Async function to extract balance sheet
+async def extract_balance_sheet(html_dataframe, year, season):
     # Season to date mapping
     season_to_date = {1: "03月31日", 2: "06月30日", 3: "09月30日", 4: "12月31日"}
     date_col = f"{year}年{season_to_date[season]}"
@@ -79,52 +80,90 @@ async def extract_report_data(html_dataframe, year, season, report_type):
     return data_dict
 
 # Async function to crawl financial report
-@rate_limited(1)  # 1 request per second
-async def crawl_financial_report(url, ticker_symbol, year, season):
+@rate_limited
+async def crawl_financial_report(ticker_symbol, year, season, report_type):
+    # URLs for different types of financial reports
+    report_urls = {
+        "balance_sheet":    "https://mops.twse.com.tw/mops/web/ajax_t164sb03",
+        "income_statement": "https://mops.twse.com.tw/mops/web/ajax_t164sb04",
+        "cash_flow":        "https://mops.twse.com.tw/mops/web/ajax_t164sb05"
+    }
+    url = report_urls[report_type]
+
+    logging.debug(f"Starting request for {url}, ticker: {ticker_symbol}, year: {year}, season: {season}")
+
     form_data = {
         'encodeURIComponent': 1,
         'step': 1,
         'firstin': 1,
         'off': 1,
-        'co_id': stock_number,
+        'co_id': ticker_symbol,
         'year': year,
         'season': season,
     }
     headers = {
-        'User-Agent': 'Your User Agent'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'
     }
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=form_data, headers=headers) as response:
+            logging.debug(f"Received response for {url}, status: {response.status}")
             if response.status != 200:
                 logging.error(f"Failed to retrieve data: Status code {response.status}")
                 return None
             try:
                 text = await response.text()
-                html_dataframe = pd.read_html(StringIO(text))[1].fillna("")
+                html_dataframe = pandas.read_html(StringIO(text))[1].fillna("")
                 return html_dataframe
             except Exception as e:
                 logging.error(f"Error occurred while parsing HTML: {e}")
                 return None
 
+# Async function to store data into database
+async def store_data(data):
+    print(data)
+
+# Function to process each report
+async def process_report(ticker_symbol, year, season, report_type):
+    async with semaphore:
+        logging.debug(f"Processing report for ticker {ticker_symbol}, year: {year}, season: {season}, type: {report_type}")
+        html_dataframe = await crawl_financial_report(ticker_symbol, year, season, report_type)
+        if html_dataframe is not None:
+            extracted_data = {}
+            if report_type == "balance_sheet":
+                extracted_data = await extract_balance_sheet(html_dataframe, year, season)
+            elif report_type == "income_statement":
+                pass
+            elif report_type == "cash_flow":
+                pass
+            logging.debug(f"Completed data extraction for {ticker_symbol}, year: {year}, season: {season}")
+            await store_data(extracted_data)
+
 # Function to schedule crawling tasks
 def schedule_crawling_tasks(scheduler):
-    report_types = ["balance_sheet", "income_statement", "cash_flow"]
-    current_year = datetime.now().year
+    logging.info("Scheduling crawling tasks")
+    #report_types = ["balance_sheet", "income_statement", "cash_flow"]
+    report_types = ["balance_sheet"]
+    #all_ticker_symbols = ["2330", "2317", "2454", "3008", "1301", "1303", "1326", "2308"]
+    all_ticker_symbols = ["2330", "2317"]
+    current_year = datetime.datetime.now().year
     for ticker_symbol in all_ticker_symbols:  # List of all ticker symbols
         for report_type in report_types:
-            for year in range(current_year, 1999, -1):  # Historical data
+            for year in range(current_year, 2020, -1):  # Historical data
                 for season in range(1, 5):
+                    #logging.debug(f"Scheduled task for report type {report_type}, ticker {ticker_symbol}, year {year}, season {season}")
                     scheduler.add_job(
-                        crawl_financial_report, 
-                        args=(REPORT_URLS[report_type], ticker_symbol, year, season)
+                        process_report,
+                        args=(ticker_symbol, year, season, report_type)
                     )
 
 # Main function to start the crawler
 async def main():
-    scheduler = AsyncIOScheduler()
+    loop = asyncio.get_event_loop()
+    scheduler = AsyncIOScheduler(event_loop=loop)
     schedule_crawling_tasks(scheduler)
     scheduler.start()
 
+    logging.info("Crawler started")
     # Keep the script running
     while True:
         await asyncio.sleep(60)
---
+++ utils/crawl_financial_report_aiohttp.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# TradeWiSE Project
+#
+# This file is part of the TradeWiSE project, an automated trading and financial analysis platform.
+# It is licensed under the Mozilla Public License 2.0 (MPL 2.0), which allows for wide use and modification
+# while ensuring that enhancements and modifications remain available to the community.
+#
+# You can find the MPL 2.0 license in the root directory of the project or at https://www.mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2023 by wildfootw <wildfootw@wildfoo.tw>
+#
+
+import aiohttp
+import pandas
+from io import StringIO
+import os
+import asyncio
+import logging
+
+# Set up logging
+log_level = os.environ.get("LOG_LEVEL", "DEBUG").upper()
+logging.basicConfig(level=log_level, format="%(asctime)s %(levelname)s %(message)s")
+
+# function to extract balance sheet
+async def extract_balance_sheet(html_dataframe, year, season):
+    # Season to date mapping
+    season_to_date = {1: "03月31日", 2: "06月30日", 3: "09月30日", 4: "12月31日"}
+    date_col = f"{year}年{season_to_date[season]}"
+
+    # Construct column names based on year and season
+    year_season_col = f"民國{year}年第{season}季"
+
+    # Change column name according to DataFrame structure
+    key_col_name = html_dataframe[year_season_col]["單位：新台幣仟元"]["會計項目"].columns[0]
+
+    # Selecting the key and value columns
+    keys = html_dataframe[year_season_col]["單位：新台幣仟元"]["會計項目"][key_col_name]
+    values = html_dataframe[year_season_col]["單位：新台幣仟元"][date_col]["金額"]
+
+    # Creating a dictionary from the selected columns
+    data_dict = dict(zip(keys, values))
+    return data_dict
+
+# function to crawl financial report
+async def crawl_financial_report(ticker_symbol, year, season, report_type):
+    # URLs for different types of financial reports
+    report_urls = {
+        "balance_sheet":    "https://mops.twse.com.tw/mops/web/ajax_t164sb03",
+        "income_statement": "https://mops.twse.com.tw/mops/web/ajax_t164sb04",
+        "cash_flow":        "https://mops.twse.com.tw/mops/web/ajax_t164sb05"
+    }
+    url = report_urls[report_type]
+
+    logging.debug(f"Starting request for {url}, ticker: {ticker_symbol}, year: {year}, season: {season}")
+
+    form_data = { 'encodeURIComponent': 1, 'step': 1, 'firstin': 1, 'off': 1, 'co_id': ticker_symbol, 'year': year, 'season': season, }
+    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0' }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=form_data, headers=headers) as response:
+            logging.debug(f"Received response for {url}, status: {response.status}")
+            if response.status != 200:
+                logging.error(f"Failed to retrieve data: Status code {response.status}")
+                return None
+            try:
+                text = await response.text()
+                html_dataframe = pandas.read_html(StringIO(text))[1].fillna("")
+                return html_dataframe
+            except Exception as e:
+                logging.error(f"Error occurred while parsing HTML: {e}")
+                return None
+
+async def main():
+    ticker_symbol = "2330"
+    year = 111
+    season = 4
+    report_type = "balance_sheet"
+    logging.debug(f"Processing report for ticker {ticker_symbol}, year: {year}, season: {season}, type: {report_type}")
+    html_dataframe = await crawl_financial_report(ticker_symbol, year, season, report_type)
+    if html_dataframe is not None:
+        extracted_data = {}
+        if report_type == "balance_sheet":
+            extracted_data = await extract_balance_sheet(html_dataframe, year, season)
+        elif report_type == "income_statement":
+            pass
+        elif report_type == "cash_flow":
+            pass
+        logging.debug(f"Completed data extraction for {ticker_symbol}, year: {year}, season: {season}")
+        print(extracted_data)
+
+if __name__ == "__main__":
+    asyncio.run(main())
