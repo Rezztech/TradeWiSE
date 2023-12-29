03417413b62ba2b3d334fa35b20718306dcf1d42
soyccan <soyccan@gmail.com>
2023-12-29 18:11:58 +0800

    Make telegram-bot more structured

--- services/telegram-bot/src/main.py
+++ services/telegram-bot/src/main.py
@@ -12,19 +12,80 @@
 #
 # Copyright (c) 2023 by wildfootw <wildfootw@wildfoo.tw>
 #
+from __future__ import annotations
 
 import logging
 import os
+from typing import TYPE_CHECKING, cast
 
 import requests
-from telegram import Update
+from telegram import Bot, BotCommand, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-telegram_bot_auth_user_id = os.getenv('TELEGRAM_BOT_AUTH_USER_ID')
+if TYPE_CHECKING:
+    from telegram.ext import Application
+
+
+def get_env_or_raise(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise ValueError(f'Environment variable "{name}" not set')
+    return value
+
+telegram_bot_token = get_env_or_raise('TELEGRAM_BOT_TOKEN')
+telegram_bot_auth_user_id = get_env_or_raise('TELEGRAM_BOT_AUTH_USER_ID')
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+class _BaseCommand:
+    command: str
+    description: str
+
+    def to_bot_command(self) -> BotCommand:
+        return BotCommand(self.command, self.description)
+
+    @staticmethod
+    async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        ...
+
+class StartCommand(_BaseCommand):
+    command = "start"
+    description = "Starts the bot"
+
+    @staticmethod
+    async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if not await is_user_authorized(update, context):
+            return
+        await update.message.reply_text("Welcome to TradeWiSE! Your trading assistant.")
+
+class StockCommand(_BaseCommand):
+    command = "stock"
+    description = "Get stock price"
+
+    @staticmethod
+    async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        if not await is_user_authorized(update, context):
+            return
+        args = context.args
+        if not args:
+            await update.message.reply_text("Please provide a stock symbol. Usage: /stock <symbol>")
+            return
+        symbol = args[0]
+        data = await StockCommand.get_stock_price(symbol)
+        await update.message.reply_text(str(data))
+
+    @staticmethod
+    async def get_stock_price(symbol: str):
+        response = requests.get(f'http://fugle-market-data:80/price/{symbol}')
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {'error': 'Unable to fetch data'}
+
+_commands: list[_BaseCommand] = [StartCommand(), StockCommand()]
+
 
 async def is_user_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
@@ -33,38 +94,20 @@ async def is_user_authorized(update: Update, context: ContextTypes.DEFAULT_TYPE)
         return False
     return True
 
-async def get_stock_price(symbol: str):
-    response = requests.get(f'http://fugle-market-data:80/price/{symbol}')
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return {'error': 'Unable to fetch data'}
+async def post_init(application: Application) -> None:
+    await cast(Bot, application.bot).set_my_commands(
+        [cmd.to_bot_command() for cmd in _commands]
+    )
 
-# Define command handlers
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not await is_user_authorized(update, context):
-        return
-    await update.message.reply_text("Welcome to TradeWiSE! Your trading assistant.")
-
-async def stock(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not await is_user_authorized(update, context):
-        return
-    args = context.args
-    if not args:
-        await update.message.reply_text("Please provide a stock symbol. Usage: /stock <symbol>")
-        return
-    symbol = args[0]
-    data = await get_stock_price(symbol)
-    await update.message.reply_text(str(data))
-
-# Main function to setup the bot
-#async def main():
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(telegram_bot_token).build()
+def main():
+    application = ApplicationBuilder().token(telegram_bot_token).post_init(post_init).build()
 
     # Register handlers
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('stock', stock))
+    for cmd in _commands:
+        application.add_handler(CommandHandler(cmd.command, cmd.handle))
 
     # Start the bot
     application.run_polling()
+
+if __name__ == '__main__':
+    main()
